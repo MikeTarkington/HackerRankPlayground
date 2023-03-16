23a0334fd1c879e8d5484950476ee0aa43c5135c
Mike Tarkington <tarkmike@gmail.com>
2023-03-16 16:54:44 +0000

    Solving factorial problem with built in funtcion an naive iteration

--- extralongfactorial.py
+++ extralongfactorial.py
@@ -1,1 +1,17 @@
-https://www.hackerrank.com/challenges/extra-long-factorials/problem?isFullScreen=true
+import math
+
+# https://www.hackerrank.com/challenges/extra-long-factorials/problem?isFullScreen=true
+
+# n * n-1 * n-2 ...
+
+def extraLongFactorials(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact = fact * i
+    return fact
+
+print(extraLongFactorials(30))
+
+# built in function makes it easy but
+# the above solves as problem was probably intended
+print(math.factorial(30))
